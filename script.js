45d5d73550f01eff26e32a524d38e174ffee2919
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-26 15:37:38 +0100

    feat: Add grid creation logic
    Added functionality to the button, replacing the default grid with a new
    one with the user speicified amount of rows and columns.

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ let gridValue;
 
 function getValue() {
   gridValue = prompt("How big is your penis?");
-  console.log(gridValue);
-  return gridValue;
+  document.querySelector(".container").innerHTML = "";
+  for(let j = 0; j < gridValue; j++) {
+    const row = document.createElement("div");
+    container.appendChild(row);
+    for(let i = 0; i < gridValue; i++) {
+      const square = document.createElement("div");
+      square.style.border = "1px solid gray";
+      row.appendChild(square);
+      square.addEventListener("mouseenter", function() {
+        square.classList.add("colored");
+      });
+    }
+  }
 }
 
 button.addEventListener("click", getValue);
